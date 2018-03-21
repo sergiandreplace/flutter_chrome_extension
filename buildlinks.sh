759572ed9194b653a0466b31aceb7aee4e1e435c
Sergi Martínez <sergi.martinez@gmail.com>
2018-03-21 20:59:02 +0100

    autogenerate script for class urls

--- buildlinks.sh
+++ buildlinks.sh
@@ -14,7 +14,42 @@ else
     SED=gsed
 fi
 
-folder=~/dev/flutter/flutter/packages/flutter/lib/src
+##save directory
+pushd .
+
+## delete just in case it exists
+rm -rf flutter_temp
+
+## download lib/src from Flutter github
+echo '### Downloading Flutter source files ###'
+
+git init flutter_temp
+cd flutter_temp
+git remote add origin git@github.com:flutter/flutter.git
+git config core.sparseCheckout true 
+echo "packages/flutter/lib/src/" >> .git/info/sparse-checkout 
+git pull origin master 
+
+popd
+
+# extract class names and lines into json body
+
+echo '### Building urls ###'
+
+
+folder=./flutter_temp/packages/flutter/lib/src
 
 grep -R -n "^ *class [A-Z]" $folder |
-$SED -E  's|class\s(\S*)\s.*|\1|g; s|^([^:]*):([^:]*):([^:]*)$|"\3":"\1#L\2",|gm'
+$SED -E 's|class\s(\S*)\s.*|\1|g; s|^./flutter_temp/packages/flutter/lib/src/([^:]*):([^:]*):([^:]*)$|    "\3": "\1#L\2",|gm;  s|(<[A-Z]+?>)||g' |
+sort |
+head -c -2 > urls.json
+
+# insert json body into template
+
+sed -E "s/CLASS_TEMPLATES/$(sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' urls.json | tr -d '\n')/" < urlmatcher_template.js > extension/urlmatcher.js
+
+# cleaning up
+echo '### cleaning up ###'
+
+rm -rf flutter_temp
+rm urls.json
